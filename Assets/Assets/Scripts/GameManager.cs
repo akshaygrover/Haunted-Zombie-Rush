97e7c7a3eedf7150b214a0d1d75388041403c170
akshaygrover <akshaygrover588@gmail.com>
2017-01-24 19:27:55 -0500

    Simple Platform Game
    Made in unity 3D

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+
+	[SerializeField] private GameObject mainMenu;
+
+	private bool playerActive = false;
+	private bool gameOver = false;
+	private bool gameStarted = false;
+
+	public bool PlayerActive {
+		get{return playerActive; }
+	}
+
+	public bool GameOver {
+
+		get{return gameOver; }
+	}
+	public bool GameStarted{
+
+		get{return gameStarted; }
+	}
+
+
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+		else if(instance!=null){
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+
+		Assert.IsNotNull (mainMenu);
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayerCollide(){
+		gameOver = true;
+
+	
+	}
+
+	public void PlayerStartedGame(){
+		playerActive = true;
+	
+	}
+	public void EnterGame(){
+
+		mainMenu.SetActive (false);
+		gameStarted = true;
+
+
+	}
+}
---
+++ Assets/Assets/Scripts/Objects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour {
+
+	[SerializeField] private float objectSpeed =1;
+	[SerializeField] private float resetPosition = 34.10944f;
+	[SerializeField] private float startPosition = -61.6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+
+		if (!GameManager.instance.GameOver) {
+			transform.Translate (Vector3.right * (objectSpeed * Time.deltaTime));
+
+			if (transform.localPosition.x >= resetPosition) {
+				Vector3 newPos = new Vector3 (startPosition, transform.position.y, transform.position.z);
+				transform.position = newPos;
+			}
+		}
+}
+
+
+}
